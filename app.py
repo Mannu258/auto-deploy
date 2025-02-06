88beb2d371d09626e760a9f3cd2d29dfb3c69aab
Your Name <you@example.com>
2025-02-06 18:11:27 +0000

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask! Mannu lauda"
+    return "Hello, Flask! Mannu lauda lehsun"
 
 if __name__ == '__main__':
     app.run(debug=True)
